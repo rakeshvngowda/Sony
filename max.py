3d171d73290852db4d75c2c890c29070a571aeab
rakesh <rakeshvngowda@gmail.com>
2020-07-20 08:48:46 +0530

    this is adding decorator function

--- max.py
+++ max.py
@@ -1,3 +1,13 @@
+def decor(func):
+    def inner(x, y):
+        if y == 0:
+            print('cannot divide by zero')
+        else:
+            return func(x, y)
+    return inner()
+  
+@decor  
 def div(a, b):
-    return a / b
+    print(a / b)
+    
     
